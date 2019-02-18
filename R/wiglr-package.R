ac39242106fbe2c18e37b27e5294539b156c8c58
boB Rudis <bob@rud.is>
2019-02-18 07:39:27 -0500

    R package repo initialization complete

---
+++ R/wiglr-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/wiglr>
+#' - BugReports: <https://gitlab.com/hrbrmstr/wiglr/issues>
+#' 
+#' @md
+#' @name wiglr
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
